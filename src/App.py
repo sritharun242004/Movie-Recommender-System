d8734279b4c1756ee02b83503523a92ea6d306ec
Tharun Kumar L <123816916+sritharun242004@users.noreply.github.com>
2024-07-31 20:08:38 +0200

    updated

--- src/App.py
+++ src/App.py
@@ -1,39 +1,44 @@
 import streamlit as st
 import pickle
 import requests
-
+import os
 
 def fetch_poster(movie_id):
-     url = "https://api.themoviedb.org/3/movie/{}?api_key=5c9b618a6739a3c302d0fcf82533ac8e&language=en-US".format(movie_id)
-     data=requests.get(url)
-     data=data.json()
-     poster_path = data['poster_path']
-     full_path = "https://image.tmdb.org/t/p/w500/"+poster_path
-     return full_path
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=5c9b618a6739a3c302d0fcf82533ac8e&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
 
-movies = pickle.load(open("/Users/tharunkumarl/Git Hub Repos/Movie-Recommender-System/models/movies_list.pkl", 'rb'))
-similarity = pickle.load(open("/Users/tharunkumarl/Git Hub Repos/Movie-Recommender-System/models/similarity.pkl", 'rb'))
-movies_list=movies['title'].values
+# Construct file paths relative to the script location
+base_dir = os.path.dirname(__file__)
+movies_path = os.path.join(base_dir, '..', 'models', 'movies_list.pkl')
+similarity_path = os.path.join(base_dir, '..', 'models', 'similarity.pkl')
+
+# Load the movies and similarity data
+movies = pickle.load(open(movies_path, 'rb'))
+similarity = pickle.load(open(similarity_path, 'rb'))
+movies_list = movies['title'].values
 
 st.header("Movie Recommender System")
 
-selectvalue=st.selectbox("Select movie from dropdown", movies_list)
+selectvalue = st.selectbox("Select movie from dropdown", movies_list)
+
 def recommend(movie):
-    index=movies[movies['title']==movie].index[0]
-    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector:vector[1])
-    recommend_movie=[]
-    recommend_poster=[]
+    index = movies[movies['title'] == movie].index[0]
+    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector: vector[1])
+    recommend_movie = []
+    recommend_poster = []
     for i in distance[1:6]:
-        movies_id=movies.iloc[i[0]].id
+        movie_id = movies.iloc[i[0]].id
         recommend_movie.append(movies.iloc[i[0]].title)
-        recommend_poster.append(fetch_poster(movies_id))
+        recommend_poster.append(fetch_poster(movie_id))
     return recommend_movie, recommend_poster
-
-
 
 if st.button("Show Recommend"):
     movie_name, movie_poster = recommend(selectvalue)
-    col1,col2,col3,col4,col5=st.columns(5)
+    col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(movie_name[0])
         st.image(movie_poster[0])
